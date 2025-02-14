1f7567574466a8e457c6c80b9c298a1f6fd30090
Rohan Manuel <rohanmanuel.a2022csbs@sece.ac.in>
2025-02-14 23:14:11 +0530

    feat: myjobs

--- client/app/myjobs/page.tsx
+++ client/app/myjobs/page.tsx
@@ -2,34 +2,31 @@
 import Footer from "@/Components/Footer";
 import Header from "@/Components/Header";
 import MyJob from "@/Components/JobItem/MyJob";
-import { useGlobalContext } from "@/context/globalContext";
+import { useUser } from "@clerk/nextjs";
 import { useJobsContext } from "@/context/jobsContext";
 import { Job } from "@/types/types";
 import { useRouter } from "next/navigation";
 import React, { useEffect } from "react";
 
-function page() {
+function Page() {
   const { userJobs, jobs } = useJobsContext();
-  const { isAuthenticated, loading, userProfile } = useGlobalContext();
+  const { isSignedIn, user, isLoaded } = useUser();
 
   const [activeTab, setActiveTab] = React.useState("posts");
 
-  const userId = userProfile?._id;
+  const userId = user?.id;
 
   const router = useRouter();
 
-  // Redirect to login if not authenticated
   useEffect(() => {
-    if (!loading && !isAuthenticated) {
+    if (isLoaded && !isSignedIn) {
       router.push("https://jobfindr-main.onrender.com");
     }
-  }, [isAuthenticated]);
+  }, [isSignedIn, isLoaded]);
 
-  const likedJobs = jobs.filter((job: Job) => {
-    return job.likes.includes(userId);
-  });
+  const likedJobs = jobs.filter((job: Job) => job.likes.includes(userId));
 
-  if (loading) {
+  if (!isLoaded) {
     return null;
   }
 
@@ -40,23 +37,17 @@ function page() {
       <div className="mt-8 w-[90%] mx-auto flex flex-col">
         <div className="self-center flex items-center gap-6">
           <button
-            className={`border border-gray-400 px-8 py-2 rounded-full font-medium
-          ${
-            activeTab === "posts"
-              ? "border-transparent bg-[#7263F3] text-white"
-              : "border-gray-400"
-          }`}
+            className={`border border-gray-400 px-8 py-2 rounded-full font-medium ${
+              activeTab === "posts" ? "border-transparent bg-[#7263F3] text-white" : "border-gray-400"
+            }`}
             onClick={() => setActiveTab("posts")}
           >
             My Job Posts
           </button>
           <button
-            className={`border border-gray-400 px-8 py-2 rounded-full font-medium
-          ${
-            activeTab === "likes"
-              ? "border-transparent bg-[#7263F3] text-white"
-              : "border-gray-400"
-          }`}
+            className={`border border-gray-400 px-8 py-2 rounded-full font-medium ${
+              activeTab === "likes" ? "border-transparent bg-[#7263F3] text-white" : "border-gray-400"
+            }`}
             onClick={() => setActiveTab("likes")}
           >
             Liked Jobs
@@ -76,11 +67,9 @@ function page() {
         )}
 
         <div className="my-8 grid grid-cols-2 gap-6">
-          {activeTab === "posts" &&
-            userJobs.map((job: Job) => <MyJob key={job._id} job={job} />)}
+          {activeTab === "posts" && userJobs.map((job: Job) => <MyJob key={job._id} job={job} />)}
 
-          {activeTab === "likes" &&
-            likedJobs.map((job: Job) => <MyJob key={job._id} job={job} />)}
+          {activeTab === "likes" && likedJobs.map((job: Job) => <MyJob key={job._id} job={job} />)}
         </div>
       </div>
 
@@ -89,4 +78,4 @@ function page() {
   );
 }
 
-export default page;
+export default Page;
